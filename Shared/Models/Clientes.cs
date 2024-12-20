0778023ab7c409cf4758b6f71daef5fd6f618d87
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-20 00:08:33 -0400

    Agrega Cliente

---
+++ Shared/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string? Direccion { get; set; }
+    public string? Telefono { get; set; }
+    public string? RNC { get; set; }
+}
