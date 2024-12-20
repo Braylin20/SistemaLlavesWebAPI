6a0fb702ff82a7a0a230218158e74bb8a065511a
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-20 17:45:02 -0400

    Agrega anotacion para excluir la clase del sonar cloud

--- SistemaLlavesWebAPI/Services/ProductServices.cs
+++ SistemaLlavesWebAPI/Services/ProductServices.cs
@@ -3,46 +3,48 @@ using Microsoft.EntityFrameworkCore;
 using Shared.Models;
 using SistemaLlavesWebAPI.Dal;
 using SistemaLlavesWebAPI.Interfaces;
+using System.Diagnostics.CodeAnalysis;
 
-namespace SistemaLlavesWebAPI.Services
+
+namespace SistemaLlavesWebAPI.Services;
+
+[ExcludeFromCodeCoverage]
+public class ProductServices : IProductService
 {
-    public class ProductServices : IProductService
+    private readonly Context _context;
+
+    public ProductServices(Context context)
     {
-        private readonly Context _context;
-
-        public ProductServices(Context context)
-        {
-            this._context = context;
-        }
-        public async Task<List<Productos>> GetAsync()
-        {
-            return await _context.Productos.ToListAsync();
-        }
-        public async Task<bool> AddAsync(Productos producto)
-        {
-            _context.Productos.Add(producto);
-            return await _context.SaveChangesAsync() > 0;
-        }
-        public async Task<Productos> PutAsync( Productos producto)
-        {
-            var result = _context.Productos.Update(producto);
-            await _context.SaveChangesAsync();
-            return result.Entity;
-
-        }
-        public async Task<Productos?> DeleteAsync(int id)
-        {
-            var producto = await _context.Productos.FindAsync(id);
-
-            if (producto is not null)
-            {
-                _context.Remove(producto);
-                await _context.SaveChangesAsync();
-            }
-
-            return producto;
-        }
-
+        this._context = context;
+    }
+    public async Task<List<Productos>> GetAsync()
+    {
+        return await _context.Productos.ToListAsync();
+    }
+    public async Task<bool> AddAsync(Productos producto)
+    {
+        _context.Productos.Add(producto);
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task<Productos> PutAsync( Productos producto)
+    {
+        var result = _context.Productos.Update(producto);
+        await _context.SaveChangesAsync();
+        return result.Entity;
 
     }
+    public async Task<Productos?> DeleteAsync(int id)
+    {
+        var producto = await _context.Productos.FindAsync(id);
+
+        if (producto is not null)
+        {
+            _context.Remove(producto);
+            await _context.SaveChangesAsync();
+        }
+
+        return producto;
+    }
+
+
 }
