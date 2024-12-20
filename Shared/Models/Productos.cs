b0e9191ddd26fdad7a4b66cf768969f1322070cc
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-20 15:07:53 -0400

    Agrega Relaciones entre los distintos modelos

--- Shared/Models/Productos.cs
+++ Shared/Models/Productos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +20,16 @@ public class Productos
     public double? Itbis { get; set; } = 0.18;
     public double? Descuento { get; set; }
 
+    [ForeignKey("CategoriaId")]
+    public Categorias? Categoria {  get; set; }
     public int CategoriaId {  get; set; }
+
+    [ForeignKey("ProveedorId")]
+    public Proveedores? Proveedor { get; set; }
     public int ProveedorId { get; set; }
+
+    [ForeignKey("GarantiaId")]
+    public Garantias? Garantia {  get; set; }
     public int? GarantiaId { get; set; }
 
 }
