52e9f1052b2c74f92bb268f5677f356d4cc3d08d
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-20 19:28:28 -0400

    Add Interface PuchaseService

---
+++ SistemaLlavesWebAPI/Interfaces/IPuchaseService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Models;
+
+namespace SistemaLlavesWebAPI.Interfaces;
+
+public interface IPuchaseService
+{
+    public Task<List<Compras>> GetAsync();
+    public Task<bool> AddAsync(Compras compra);
+    public Task<Compras> PutAsync(Compras compra);
+    public Task<Compras> DeleteAsync(int id);
+}
