3cb0133008572eb4d687d4cec553896187aa948d
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-20 16:38:05 -0400

    Aprega metodos para implementar

--- SistemaLlavesWebAPI/Services/ProductServices.cs
+++ SistemaLlavesWebAPI/Services/ProductServices.cs
@@ -1,6 +1,28 @@
-﻿namespace SistemaLlavesWebAPI.Services
+﻿using Shared.Models;
+using SistemaLlavesWebAPI.Interfaces;
+
+namespace SistemaLlavesWebAPI.Services
 {
-    public class ProductServices
+    public class ProductServices : IProductService
     {
+        public Task<Productos> AddAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Productos> DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Productos>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Productos> PutAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
