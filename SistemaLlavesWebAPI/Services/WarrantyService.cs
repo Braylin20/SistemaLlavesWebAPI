ab21763344fd456c1eab63e413e91f2b06d6c267
Eliezer Terrero Rojas <eliezerterrero12@gmail.com>
2024-12-20 19:30:30 -0400

    Add WarrantyService

---
+++ SistemaLlavesWebAPI/Services/WarrantyService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+using SistemaLlavesWebAPI.Dal;
+using SistemaLlavesWebAPI.Interfaces;
+
+namespace SistemaLlavesWebAPI.Services
+{
+    public class WarrantyService(Context context) : IWarrantyService
+    {
+        private readonly Context _context = context;
+
+        public async Task<List<Garantias>> GetAsync()
+        {
+            return await _context.Garantias.ToListAsync();
+        }
+        public async Task<bool> AddAsync(Garantias garantia)
+        {
+            await _context.Garantias.AddAsync(garantia);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async Task<Garantias> PutAsync(Garantias garantia)
+        {
+            var result = _context.Garantias.Update(garantia);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+        public async Task<Garantias> DeleteAsync(int id)
+        {
+            var warranty = await _context.Garantias.FindAsync(id) ?? throw new KeyNotFoundException($"Warranty with ID {id} was not found.");
+            _context.Garantias.Remove(warranty);
+            await _context.SaveChangesAsync();
+
+            return warranty;
+        }
+    }
+}
