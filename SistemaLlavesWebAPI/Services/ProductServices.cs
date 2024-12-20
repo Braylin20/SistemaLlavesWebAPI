e6fdd1c20081d1a6d807fdc2a3b3ab5bf916415d
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-20 17:04:57 -0400

    Agrega metodo Get y Add

--- SistemaLlavesWebAPI/Services/ProductServices.cs
+++ SistemaLlavesWebAPI/Services/ProductServices.cs
@@ -1,28 +1,36 @@
-﻿using Shared.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+using SistemaLlavesWebAPI.Dal;
 using SistemaLlavesWebAPI.Interfaces;
 
 namespace SistemaLlavesWebAPI.Services
 {
     public class ProductServices : IProductService
     {
-        public Task<Productos> AddAsync()
+        private readonly Context _context;
+
+        public ProductServices(Context context)
+        {
+            this._context = context;
+        }
+        public async Task<List<Productos>> GetAsync()
+        {
+            return await _context.Productos.ToListAsync();
+        }
+        public async Task<bool> AddAsync(Productos producto)
+        {
+            _context.Productos.Add(producto);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public Task<Productos> PutAsync()
         {
             throw new NotImplementedException();
         }
-
         public Task<Productos> DeleteAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<Productos>> GetAsync()
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<Productos> PutAsync()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
