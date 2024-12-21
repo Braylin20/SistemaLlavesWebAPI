3b94dd20badfc5425266ef5592408aa4cfed43ae
Steven Candelario <steven_candelario@ucne.edu.do>
2024-12-21 19:14:08 -0400

    Add ProviderService Interface

---
+++ SistemaLlavesWebAPI/Interfaces/IProviderService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Models;
+
+namespace SistemaLlavesWebAPI.Interfaces;
+
+public interface IProviderService
+{
+    Task<List<Proveedores>> GetAsync();
+    Task<bool> AddAsync(Proveedores proveedores);
+    Task<Proveedores> PutAsync(Proveedores proveedores);
+    Task<Proveedores> DeleteAsync(int proveedorId);
+}
