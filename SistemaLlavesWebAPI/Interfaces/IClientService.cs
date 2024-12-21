c0be9882deb4f9620249a6585b1e8f17b24e2a17
Steven Candelario <steven_candelario@ucne.edu.do>
2024-12-21 19:13:41 -0400

    Add ClientService Interface

---
+++ SistemaLlavesWebAPI/Interfaces/IClientService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Models;
+
+namespace SistemaLlavesWebAPI.Interfaces;
+
+public interface IClientService
+{
+    public Task<List<Clientes>> GetAsync();
+    public Task<bool> AddAsync(Clientes clientes);
+    public Task<Clientes> PutAsync(Clientes clientes);
+    public Task<Clientes> DeleteAsync(int clienteId);
+}
