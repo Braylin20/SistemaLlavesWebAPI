951f0b397a0d9ecfc7d22b415dac5f030441e037
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-21 12:10:46 -0400

    Change method put to allow null return

--- SistemaLlavesWebAPI/Services/ProductServices.cs
+++ SistemaLlavesWebAPI/Services/ProductServices.cs
@@ -21,23 +21,34 @@ public class ProductServices(Context _context) : IProductService
         _context.Productos.Add(producto);
         return await _context.SaveChangesAsync() > 0;
     }
-    public async Task<Productos> PutAsync(Productos producto)
+    public async Task<Productos?> PutAsync(Productos producto)
     {
+        var existingProducto = await _context.Productos.FindAsync(producto.ProductoId);
+
+        if (existingProducto is null) return existingProducto;
+
+        _context.Entry(existingProducto).State = EntityState.Detached;
+
         var result = _context.Productos.Update(producto);
         await _context.SaveChangesAsync();
         return result.Entity;
 
     }
-    public async Task<Productos> DeleteAsync(int id)
+    public async Task<Productos?> DeleteAsync(int id)
     {
-        var producto = await _context.Productos.FindAsync(id) ??
-            throw new KeyNotFoundException("The product was not found");
-
-        _context.Remove(producto);
-        await _context.SaveChangesAsync();
+        var producto = await _context.Productos.FindAsync(id);
+        if(producto != null)
+        {
+            _context.Remove(producto);
+            await _context.SaveChangesAsync();
+        }
+        
 
         return producto;
     }
 
-
+    public async Task<Productos?> GetById(int id)
+    {
+        return  await _context.Productos.FindAsync(id);
+    }
 }
