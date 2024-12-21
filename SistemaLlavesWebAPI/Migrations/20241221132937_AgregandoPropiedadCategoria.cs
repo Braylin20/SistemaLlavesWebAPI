ab6cde074196992e46f88f778d9e1cc983834b16
Yudelka Elister Guillén Durán <yudelka_guillen@ucne.edu.do>
2024-12-21 10:21:10 -0400

    Add seccond migration AgregandoPropiedadCategoria

---
+++ SistemaLlavesWebAPI/Migrations/20241221132937_AgregandoPropiedadCategoria.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaLlavesWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoPropiedadCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "Categorias",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "Categorias");
+        }
+    }
+}
