cf04a8c9655a2d0a46739b755cf73fbcfd4fdfbe
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-22 14:36:57 -0400

    Modify method Put to detached the existing entitty

--- SistemaLlavesWebAPI/Services/ProductServices.cs
+++ SistemaLlavesWebAPI/Services/ProductServices.cs
@@ -11,7 +11,7 @@ namespace SistemaLlavesWebAPI.Services;
 [ExcludeFromCodeCoverage]
 public class ProductServices(Context _context) : IProductService
 {
-    
+
     public async Task<List<Productos>> GetAsync()
     {
         return await _context.Productos.ToListAsync();
@@ -23,25 +23,28 @@ public class ProductServices(Context _context) : IProductService
     }
     public async Task<bool> PutAsync(Productos producto)
     {
-        _context.Update(producto);
-        _context.Entry(producto).State = EntityState.Detached;
-        return await _context.SaveChangesAsync() > 0 ;
+        var existingEntity = await _context.Productos.FindAsync(producto.ProductoId);
+        if (existingEntity is null) return false;
 
+        _context.Entry(existingEntity).State = EntityState.Detached; 
+
+        _context.Update(producto);
+        return await _context.SaveChangesAsync() > 0;
     }
     public async Task<Productos?> DeleteAsync(int id)
     {
         var producto = await _context.Productos.FindAsync(id);
-        if(producto != null)
+        if (producto != null)
         {
             _context.Remove(producto);
             await _context.SaveChangesAsync();
         }
-        
+
         return producto;
     }
 
     public async Task<Productos?> GetById(int id)
     {
-        return  await _context.Productos.FindAsync(id);
+        return await _context.Productos.FindAsync(id);
     }
 }
