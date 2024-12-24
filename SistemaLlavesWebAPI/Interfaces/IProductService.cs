0fd89353fb907999030356a7ea5ea8aeeb90c50f
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-24 14:16:04 -0400

    Add ? to allow null values

--- SistemaLlavesWebAPI/Interfaces/IProductService.cs
+++ SistemaLlavesWebAPI/Interfaces/IProductService.cs
@@ -7,7 +7,7 @@ public interface IProductService
     public Task<List<Productos>> GetAsync();
     public Task <bool> AddAsync(Productos producto);
     public Task<bool> PutAsync(Productos producto);
-    public Task<Productos> DeleteAsync(int id);
+    public Task<Productos?> DeleteAsync(int id);
     public Task<Productos?> GetById(int id);
    
 }
