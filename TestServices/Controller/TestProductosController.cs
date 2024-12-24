0a703551776920187d0b7278e05eed908345e570
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-24 11:44:59 -0400

    Add ProductosControllerTest

---
+++ TestServices/Controller/TestProductosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Shared.Models;
+using SistemaLlavesWebAPI.Controllers;
+using SistemaLlavesWebAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServices.Controller;
+
+public class TestProductosController
+{
+    private readonly Mock<IProductService> _mockProductService;
+    private readonly ProductosController _controller;
+
+
+    public TestProductosController()
+    {
+        _mockProductService = new Mock<IProductService>();
+        _controller = new ProductosController(_mockProductService.Object);
+    }
+
+    [Fact]
+    public async Task GetProductos_ShouldReturnListOfProducts()
+    {
+        var productList = new List<Productos> {
+            new() { ProductoId =1 , Cantidad= 10},
+            new() { ProductoId =2 , Cantidad= 10}
+        };
+
+        _mockProductService.Setup(s => s.GetAsync()).ReturnsAsync(productList);
+
+        var result = await _controller.GetProductos();
+
+        var okResult = Assert.IsType<ActionResult<IEnumerable<Productos>>>(result);
+        Assert.Equal(productList, okResult.Value);
+    }
+}
+
