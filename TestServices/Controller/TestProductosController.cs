0467f0c4c4f86f6177fadf81166f1250d4a9f0d0
Braylin20 <braylin_vasquez@ucne.edu.do>
2024-12-24 13:08:57 -0400

    Add Testing of Product

--- TestServices/Controller/TestProductosController.cs
+++ TestServices/Controller/TestProductosController.cs
@@ -38,5 +38,104 @@ public class TestProductosController
         var okResult = Assert.IsType<ActionResult<IEnumerable<Productos>>>(result);
         Assert.Equal(productList, okResult.Value);
     }
+
+    [Fact]
+    public async Task GetProductos_ShouldReturnProductById()
+    {
+        var product = new Productos() { ProductoId = 1, Cantidad = 10 };
+
+
+        _mockProductService.Setup(s => s.GetById(product.ProductoId)).ReturnsAsync(product);
+
+        var result = await _controller.GetProductos(product.ProductoId);
+
+        var okResult = Assert.IsType<ActionResult<Productos>>(result);
+        Assert.Equal(product, okResult.Value);
+    }
+
+    [Fact]
+    public async Task PostProductos_ShouldCreateAnAction_WhenIsSuccessful()
+    {
+        // Arrange
+        var producto = new Productos { ProductoId = 1, Precio= 2.2 };
+        _mockProductService.Setup(s => s.AddAsync(producto)).ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.PostProductos(producto);
+
+        // Assert
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        Assert.Equal("GetProductos", createdResult.ActionName);
+    }
+
+    [Fact]
+    public async Task PostProductos_ReturnsBadRequest_WhenAddFails()
+    {
+        // Arrange
+        var producto = new Productos { ProductoId = 1, Precio = 20.2 };
+        _mockProductService.Setup(s => s.AddAsync(producto)).ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.PostProductos(producto);
+
+        // Assert
+        Assert.IsType<BadRequestResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task PutProductos_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        var producto = new Productos { ProductoId = 1, Precio = 20.2 };
+
+        _mockProductService.Setup(s => s.PutAsync(producto)).ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.PutProductos(1, producto);
+
+        // Assert
+        Assert.IsType<OkResult>(result);
+    }
+
+    [Fact]
+    public async Task PutCompras_ReturnsBadRequest_WhenIdsDoNotMatch()
+    {
+        // Arrange
+        var producto = new Productos { ProductoId = 1, Precio = 20.2 };
+
+        // Act
+        var result = await _controller.PutProductos(2, producto);
+
+        // Assert
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public async Task DeleteCompras_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        var producto = new Productos {ProductoId = 1, Precio = 20.2 };
+        _mockProductService.Setup(s => s.DeleteAsync(1)).ReturnsAsync(producto);
+
+        // Act
+        var result = await _controller.DeleteProductos(1);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(producto, okResult.Value);
+    }
+
+    [Fact]
+    public async Task DeleteCompras_ReturnsNotFound_WhenCompraDoesNotExist()
+    {
+        // Arrange
+        _mockProductService?.Setup(s => s.DeleteAsync(1)).ReturnsAsync((Productos?)null);
+
+        // Act
+        var result = await _controller.DeleteProductos(1);
+
+        // Assert
+        Assert.IsType<NotFoundObjectResult>(result);
+    }
 }
 
