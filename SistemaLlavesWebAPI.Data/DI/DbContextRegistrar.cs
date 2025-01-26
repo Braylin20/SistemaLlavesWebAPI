dd5e800d7cc512be41ab25116d1a78468d76b2e5
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-01-25 22:29:00 -0400

    add ;

--- SistemaLlavesWebAPI.Data/DI/DbContextRegistrar.cs
+++ SistemaLlavesWebAPI.Data/DI/DbContextRegistrar.cs
@@ -17,7 +17,7 @@ namespace SistemaLlavesWebAPI.Data.DI
         public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services, IConfiguration configuration)
         {
             // Usa la configuración para obtener la cadena de conexión
-            var connectionString = configuration.GetConnectionString("ConStr")
+            var connectionString = configuration.GetConnectionString("ConStr");
             // Configura el DbContextFactory con la cadena de conexión
             services.AddDbContextFactory<Context>(options =>
                 options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 20))));
